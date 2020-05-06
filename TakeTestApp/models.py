29bbf37932deadf00bc09956f54dc0cc73ae105b
kiran <sai8kiran99@gmail.com>
2020-05-06 16:17:57 +0530

    Code Cleanup

--- TakeTestApp/models.py
+++ TakeTestApp/models.py
@@ -1,13 +1,1 @@
 from django.db import models
-from django.utils import timezone
-from django.contrib.auth.models import User
-
-
-class testPost(models.Model):
-    title = models.CharField(max_length=100)
-    additionalInfo = models.CharField(max_length=100)
-    date_posted = models.DateTimeField(default=timezone.now)
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.title

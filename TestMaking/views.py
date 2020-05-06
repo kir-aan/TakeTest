61bafb4c0a34a2c90076c03e434b5dd27148d228
kiran <sai8kiran99@gmail.com>
2020-05-06 17:31:44 +0530

    Removed the need to pass test details via url

--- TestMaking/views.py
+++ TestMaking/views.py
@@ -2,27 +2,27 @@ from django.shortcuts import render, redirect
 from Test.models import TestInfo
 
 
-def MakeTest(request,organisationName,testName,pMarks,nMarks):
-    p = 'media/Tests/' + organisationName + '/' + testName + '/' + testName + '.txt'
-    file1 = open(p, 'r')
-    lines = file1.readlines()
-    file1.close()
-    ques = []
-    options = []
-    ans = []
-    for line in lines:
-        if "Q:" in line:
-            ques.append(line)
-        if "opt" in line:
-            options.append(line.replace('*', ''))
-        if "*" in line:
-            ans.append(line.replace('*', '')[:-1])
-    i = 0
-    quesBank = {}
-    for qu in ques:
-        quesBank[qu] = [options[x] for x in range(i, i + 4)]
-        i = i + 4
-    return render(request, 'TestMaking/maketest.html', {'testName':testName,'quesBank': quesBank,'ans':ans,'pMarks':pMarks,'nMarks':nMarks})
-    # if request.method == 'POST':
-    #     messages.success(request, 'Dear student, your test is submited successfully')
-    #     return redirect('TakeTest-Home')
+def MakeTest2(request):
+    if "startbtn" in request.POST:
+        # currentStudent = request.user
+        currentTestObj = TestInfo.objects.get(InputTextFile=request.POST['f_name'])
+        path = 'media/'+request.POST['f_name']
+        file1 = open(path, 'r')
+        lines = file1.readlines()
+        file1.close()
+        ques = []
+        options = []
+        ans = []
+        for line in lines:
+            if "Q:" in line:
+                ques.append(line)
+            if "opt" in line:
+                options.append(line.replace('*', ''))
+            if "*" in line:
+                ans.append(line.replace('*', '')[:-1])
+        i = 0
+        quesBank = {}
+        for qu in ques:
+            quesBank[qu] = [options[x] for x in range(i, i + 4)]
+            i = i + 4
+        return render(request, 'TestMaking/maketest.html', {'testName':currentTestObj.TestName,'quesBank': quesBank,'ans':ans,'pMarks':currentTestObj.PosMarks,'nMarks':currentTestObj.NegMarks})
